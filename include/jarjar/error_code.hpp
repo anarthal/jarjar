e7511cb44ab133934050e72f6511001abe033254
Ruben Perez <rubenperez038@gmail.com>
2024-12-16 20:21:21 +0100

    Type aliases

--- include/jarjar/error_code.hpp
+++ include/jarjar/error_code.hpp
@@ -4,3 +4,16 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+
+#ifndef JARJAR_ERROR_CODE_HPP
+#define JARJAR_ERROR_CODE_HPP
+
+#include <boost/system/error_code.hpp>
+
+namespace jarjar {
+
+using error_code = boost::system::error_code;
+
+}  // namespace jarjar
+
+#endif
--- include/jarjar/string_view.hpp
+++ include/jarjar/string_view.hpp
@@ -4,3 +4,16 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+
+#ifndef JARJAR_STRING_VIEW_HPP
+#define JARJAR_STRING_VIEW_HPP
+
+#include <boost/core/detail/string_view.hpp>
+
+namespace jarjar {
+
+using string_view = boost::core::string_view;
+
+}  // namespace jarjar
+
+#endif
